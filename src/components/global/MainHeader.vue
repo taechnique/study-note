7752e3cd057335619dd421cee947e65585e604be
Kimchi-dev <tnine923@gmail.com>
2022-03-06 03:01:04 +0900

    custom for header

--- src/components/global/MainHeader.vue
+++ src/components/global/MainHeader.vue
@@ -9,12 +9,19 @@
       <div class="profile-image">
         <img :src="me.profile_image" />
       </div>
-      <ul class="spread-items">
-        <li>Home</li>
-        <li>Categories</li>
-        <li>Tags</li>
-        <li>About</li>
-      </ul>
+      <div class="author-info">
+        <span class="author">Dev-Phantom</span>
+        <span class="author-says">세상에 기여하고 싶어요.</span>
+      </div>
+      <div class="menu-item">
+        <ul class="spread-items">
+          <li><span>Home</span></li>
+          <li>Categories</li>
+          <li>Tags</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div class="search-layer"></div>
     </div>
   </div>
 </template>
@@ -25,7 +32,7 @@ export default {
     console.log(process.env)
     return {
       me: {
-        profile_image: ''
+        profile_image: 'https://github.com/Dev-Phantom/study-node/blob/main/src/assets/profile.png?raw=true'
       }
     }
   }
@@ -37,16 +44,95 @@ export default {
 
 .main-header {
   width: 70%;
-  height: 140px;
+  height: 200px;
   margin: 40px auto 20px auto;
   background-color: $main-light-color;
   border-radius: 15px;
   box-shadow: 0px 1px 30px 0 rgb(32 33 36 / 34%);
 
   & .header-item-layer {
+    display: flex;
 
-    & .spread-items {
-      list-style: none;
+    & .profile-image {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      border: #f9c6c6 4.45px solid;
+      overflow: hidden;
+      display: inline-block;
+      margin: 20px 20px;
+
+      & img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      &:hover {
+        border-color: #f35d5d;
+
+        & img {
+          -webkit-transform:scale(1.1);
+          -moz-transform:scale(1.1);
+          -o-transform:scale(1.1);
+          transform:scale(1.1);
+        }
+      }
+
+    }
+
+    & .author-info {
+      display: inline-block;
+      padding: 20px 40px;
+      height: inherit;
+      vertical-align: middle;
+
+      & .author {
+        font-size: 1.72rem;
+        font-weight: bold;
+      }
+
+      & .author-says {
+        font-style: italic !important;
+      }
+
+      & span {
+        display: block;
+        margin: 20px 0;
+      }
+    }
+
+    & .menu-item {
+      display: inline-block;
+      padding: 20px 60px;
+      width: 40%;
+
+      & .spread-items {
+        list-style: none;
+        display: flex;
+        height: 100%;
+        justify-content: space-between;
+
+        & li {
+          display: inline-block;
+          color: #717171;
+          font-size: 1.787rem;
+          cursor: pointer;
+          margin: 10px 10px;
+          padding: 0px 7px;
+
+          & span {
+            margin: auto 0px;
+          }
+        }
+      }
+
+    }
+
+    & .search-layer {
+      display: inline-block;
+      background-color: #2c3e50;
+      flex: 1;
     }
   }
 }

ec1c17d7c915c50678d9ce1e26d83e5243609931
Kimchi-dev <tnine923@gmail.com>
2022-03-10 23:46:45 +0900

    github pages api-test

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -17,7 +17,7 @@ export default {
   },
   methods: {
     getPostData: () => {
-      const url = 'https://github.com/Dev-Phantom/study-note/tree/main/src/docs'
+      const url = 'https://github.com/Dev-Phantom/study-note'
       axios.get(url).then(res => {
         console.log(res)
         this.post_content = res.data
